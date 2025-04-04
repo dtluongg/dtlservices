ebce80a0ebe58eac15668cb0420a5b1eef1d7654
dtluongg <fastdominicracing@gmail.com>
2025-04-04 22:02:21 +0700

    Cap nhat huong dan

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ProductDetail from "./component/ProductDetail/ProductDetail";
 function App() {
     return (
         <div>
-            <Router>
+            <Router basename="/dtlservices">
                 <div className="App">
                     <Routes>
                         <Route path="/" element={
